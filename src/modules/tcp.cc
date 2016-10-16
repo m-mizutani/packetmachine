b4a96f21c2df56c6ca6fda73a9f1875a7faaf53c
Masa Mizutani <mizutani@sfc.wide.ad.jp>
2016-10-16 12:24:37 +0900

    implement TCP module (only decoding header) and add TCP test

---
+++ src/modules/tcp.cc
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2016 Masayoshi Mizutani <mizutani@sfc.wide.ad.jp> All
+ * rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "../module.hpp"
+
+namespace pm {
+
+class TCP : public Module {
+ private:
+  struct tcp_header {
+    u_int16_t src_port_;  // source port
+    u_int16_t dst_port_;  // destination port
+    u_int32_t seq_;       // tcp sequence number
+    u_int32_t ack_;       // tcp ack number
+
+    // ToDo(Masa): 4 bit data field should be updated for little-endian
+    u_int8_t offset_;
+
+    u_int8_t flags_;      // flags
+    u_int16_t window_;    // window
+    u_int16_t chksum_;    // checksum
+    u_int16_t urgptr_;    // urgent pointer
+  } __attribute__((packed));
+
+  const ParamDef* p_src_port_;
+  const ParamDef* p_dst_port_;
+  const ParamDef* p_seq_;
+  const ParamDef* p_ack_;
+  const ParamDef* p_offset_;
+  const ParamDef* p_window_;
+  const ParamDef* p_chksum_;
+  const ParamDef* p_urgptr_;
+  const ParamDef* p_segment_;
+
+  // Flags
+  const ParamDef* p_flag_fin_;
+  const ParamDef* p_flag_syn_;
+  const ParamDef* p_flag_rst_;
+  const ParamDef* p_flag_push_;
+  const ParamDef* p_flag_ack_;
+  const ParamDef* p_flag_urg_;
+  const ParamDef* p_flag_ece_;
+  const ParamDef* p_flag_cwr_;
+
+  static const u_int8_t FIN  = 0x01;
+  static const u_int8_t SYN  = 0x02;
+  static const u_int8_t RST  = 0x04;
+  static const u_int8_t PUSH = 0x08;
+  static const u_int8_t ACK  = 0x10;
+  static const u_int8_t URG  = 0x20;
+  static const u_int8_t ECE  = 0x40;
+  static const u_int8_t CWR  = 0x80;
+
+
+
+ public:
+  TCP() {
+#define DEFINE_PARAM(name) \
+    this->p_ ## name ## _ = this->define_param(#name);
+
+    DEFINE_PARAM(src_port);
+    DEFINE_PARAM(dst_port);
+    DEFINE_PARAM(seq);
+    DEFINE_PARAM(ack);
+    DEFINE_PARAM(offset);
+    DEFINE_PARAM(window);
+    DEFINE_PARAM(chksum);
+    DEFINE_PARAM(urgptr);
+    DEFINE_PARAM(segment);
+
+    // Flags
+    DEFINE_PARAM(flag_fin);
+    DEFINE_PARAM(flag_syn);
+    DEFINE_PARAM(flag_rst);
+    DEFINE_PARAM(flag_push);
+    DEFINE_PARAM(flag_ack);
+    DEFINE_PARAM(flag_urg);
+    DEFINE_PARAM(flag_ece);
+    DEFINE_PARAM(flag_cwr);
+  }
+
+  void setup() {
+  }
+
+
+  mod_id decode(Payload* pd, Property* prop) {
+    auto hdr = reinterpret_cast<const struct tcp_header*>
+               (pd->retain(sizeof(struct tcp_header)));
+    if (hdr == nullptr) {   // Not enough packet size.
+      return Module::NONE;
+    }
+
+#define SET_PROP_FROM_HDR(NAME)                                         \
+    prop->retain_value(this->p_ ## NAME)->set(&(hdr->NAME), sizeof(hdr->NAME));
+    
+    SET_PROP_FROM_HDR(src_port_);
+    SET_PROP_FROM_HDR(dst_port_);
+    SET_PROP_FROM_HDR(seq_);
+    SET_PROP_FROM_HDR(ack_);
+
+    {
+      uint8_t offset = (hdr->offset_ & 0xf0) >> 2;
+      prop->retain_value(this->p_offset_)->cpy(&offset, sizeof(offset));
+    }
+
+    SET_PROP_FROM_HDR(offset_);
+    SET_PROP_FROM_HDR(window_);
+    SET_PROP_FROM_HDR(chksum_);
+    SET_PROP_FROM_HDR(urgptr_);
+
+    prop->retain_value(this->p_segment_)->set(pd->ptr(), pd->length());
+
+    
+#define SET_FLAGS(FNAME, PNAME)                                         \
+    {                                                                   \
+      byte_t f = ((hdr->flags_ & (FNAME)) > 0);                         \
+      prop->retain_value(this->p_flag_ ## PNAME ## _)->cpy(&f, sizeof(f)); \
+    }
+
+    SET_FLAGS(FIN, fin);
+    SET_FLAGS(SYN, syn);
+    SET_FLAGS(RST, rst);
+    SET_FLAGS(PUSH, push);
+    SET_FLAGS(ACK, ack);
+    SET_FLAGS(URG, urg);
+    SET_FLAGS(ECE, ece);
+    SET_FLAGS(CWR, cwr);
+    
+    mod_id next = Module::NONE;
+    return next;
+  }
+};
+
+INIT_MODULE(TCP);
+
+}   // namespace pm
---
+++ test/modules/TCP.cc
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016 Masayoshi Mizutani <mizutani@sfc.wide.ad.jp> All
+ * rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "./fixtures.hpp"
+
+TEST_F(ModuleTesterData1, TCP_packet) {
+  const pm::Property* p;
+  p = get_property(4);   // # packet #5
+
+  EXPECT_TRUE(p->has_value("TCP.src_port"));
+  EXPECT_TRUE(p->has_value("TCP.dst_port"));
+  EXPECT_TRUE(p->has_value("TCP.seq"));
+  EXPECT_TRUE(p->has_value("TCP.ack"));
+  EXPECT_TRUE(p->has_value("TCP.offset"));
+  EXPECT_TRUE(p->has_value("TCP.window"));
+  EXPECT_TRUE(p->has_value("TCP.chksum"));
+  EXPECT_TRUE(p->has_value("TCP.urgptr"));
+  EXPECT_TRUE(p->has_value("TCP.segment"));
+
+  // Flags
+  EXPECT_TRUE(p->has_value("TCP.flag_fin"));
+  EXPECT_TRUE(p->has_value("TCP.flag_syn"));
+  EXPECT_TRUE(p->has_value("TCP.flag_rst"));
+  EXPECT_TRUE(p->has_value("TCP.flag_push"));
+  EXPECT_TRUE(p->has_value("TCP.flag_ack"));
+  EXPECT_TRUE(p->has_value("TCP.flag_urg"));
+  EXPECT_TRUE(p->has_value("TCP.flag_ece"));
+  EXPECT_TRUE(p->has_value("TCP.flag_cwr"));
+
+  // TCP header values
+  EXPECT_EQ(443,         p->value("TCP.src_port").uint());
+  EXPECT_EQ(61301,       p->value("TCP.dst_port").uint());
+  EXPECT_EQ(0x310f035e,  p->value("TCP.seq").uint());
+  EXPECT_EQ(0xf597c00a,  p->value("TCP.ack").uint());
+  EXPECT_EQ(32,          p->value("TCP.offset").uint());
+
+  EXPECT_EQ(0, p->value("TCP.flag_fin").uint());
+  EXPECT_EQ(0, p->value("TCP.flag_syn").uint());
+  EXPECT_EQ(0, p->value("TCP.flag_rst").uint());
+  EXPECT_LT(0, p->value("TCP.flag_push").uint());
+  EXPECT_LT(0, p->value("TCP.flag_ack").uint());
+  EXPECT_EQ(0, p->value("TCP.flag_urg").uint());
+  EXPECT_EQ(0, p->value("TCP.flag_ece").uint());
+  EXPECT_EQ(0, p->value("TCP.flag_cwr").uint());
+  
+}
+
