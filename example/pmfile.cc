35cf12a50e93f9ea2afe7cccf80f12d6c19e4ccf
Masa Mizutani <mizutani@sfc.wide.ad.jp>
2016-11-06 15:26:54 +0900

    add a new example of PM, pmfile reading a pcap file

---
+++ example/pmfile.cc
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2016 Masayoshi Mizutani <mizutani@sfc.wide.ad.jp> All
+ * rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <iostream>
+#include "../src/packetmachine.hpp"
+
+int main(int argc, char* argv[]) {
+  pm::Machine m;
+
+  if (argc != 2) {
+    std::cout << "usage) pmfile <file_path>" << std::endl;
+    return -1;
+  }
+
+  pm::param_id ipv4_src = m.lookup_param_id("IPv4.src");
+  pm::param_id ipv4_dst = m.lookup_param_id("IPv4.dst");
+  pm::param_id tcp_src =  m.lookup_param_id("TCP.src_port");
+  pm::param_id tcp_dst =  m.lookup_param_id("TCP.dst_port");
+  pm::param_id udp_src =  m.lookup_param_id("UDP.src_port");
+  pm::param_id udp_dst =  m.lookup_param_id("UDP.dst_port");
+  pm::param_id icmp_type = m.lookup_param_id("ICMP.type");
+  pm::param_id icmp_code = m.lookup_param_id("ICMP.code");
+
+  try {
+    m.on("TCP", [&](const pm::Property& p) {
+        printf("TCP: %s:%llu > %s:%llu\n",
+               p[ipv4_src].repr().c_str(), p[tcp_src].uint(),
+               p[ipv4_dst].repr().c_str(), p[tcp_dst].uint());
+      });
+
+    m.on("UDP", [&](const pm::Property& p) {
+        printf("UDP: %s:%llu > %s:%llu\n",
+               p[ipv4_src].repr().c_str(), p[udp_src].uint(),
+               p[ipv4_dst].repr().c_str(), p[udp_dst].uint());
+      });
+
+    m.on("ICMP", [&](const pm::Property& p) {
+        printf("ICMP: %s > %s (%s:%s)\n",
+               p[ipv4_src].repr().c_str(), p[ipv4_dst].repr().c_str(),
+               p[icmp_type].repr().c_str(), p[icmp_code].repr().c_str());
+      });
+
+    m.add_pcapfile(argv[1]);
+    m.loop();
+  } catch (pm::Exception::Error &e) {
+    std::cerr << "PacketMachine Error: " << e.what() << std::endl;
+  }
+}
